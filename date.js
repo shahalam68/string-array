e7b3d4c1ee123451454c7922578e26f445fa4ffe
Soykot Alam <a.k.mshahalam68@gmail.com>
2022-07-28 01:49:08 +0600

    second

---
+++ date.js
@@ -0,0 +1,4 @@
+const myFavDate = new Date('1971-12-16');
+console.log(myFavDate);
+
+const str1 = 'Bangladesh is a beautiful country.'; console.log(str1.endsWith("country"));
---
+++ try-catch.js
@@ -0,0 +1,6 @@
+const name = 'ami';
+for(let i = 0; i< 10 i++){
+    
+}
+name = 'tumi';
+console.log(name);
